ffa4ffd683989f3458aede1fcc0ceeb19b0f13ab
Capi <developer@capi.gg>
2026-03-12 16:38:16 -0400

    feat: Fact check command

--- src/commands/TestCommand.ts
+++ src/commands/TestCommand.ts
@@ -5,37 +5,28 @@ import logger from "../utilities/Logger";
 export default class TestCommand extends SlashCommand {
   constructor() {
     super('test', 'dev command', 'Developer');
-
-    this.data.addChannelOption((o) => o.setName('channel').addChannelTypes(ChannelType.GuildText).setDescription('Select a channel').setRequired(true));
   }
 
   public async execute(interaction: ChatInputCommandInteraction, client: Client): Promise<void> {
-    const channel = interaction.options.getChannel('channel', true, [ChannelType.GuildText]);
+    await interaction.deferReply();
 
-    const container = new ContainerBuilder()
-      .addTextDisplayComponents(
-        (textDisplay) =>
-          textDisplay.setContent("You can use the buttons below to adventure instead of running those pesky application commands! This is made to be an easier way to play the game passively through Discord."),
-        (textDisplay) =>
-          textDisplay.setContent("-# If you want to know how to get a message like this set up in your Discord server you can reach out to the developer in the official Discord server!")
-      )
-      .addActionRowComponents(
-        (row) =>
-          row.addComponents(
-            new ButtonBuilder().setCustomId('explore').setLabel('Explore').setStyle(ButtonStyle.Primary),
-            new ButtonBuilder().setCustomId('attack').setLabel('Attack').setStyle(ButtonStyle.Danger),
-            new ButtonBuilder().setCustomId('flee').setLabel('Flee').setStyle(ButtonStyle.Secondary),
-          )
-      );
+    const res = await fetch('https://capi.gg/api/facts/random', {
+      headers: {
+        'Authorization': `Bearer ***REMOVED_API_KEY***`,
+        'Content-Type': 'application/json'
+      }
+    });
 
-    try {
-      await channel.send({ components: [container], flags: MessageFlags.IsComponentsV2 });
-      await interaction.reply({ content: `Sent message to ${channel}` });
-    } catch (error) {
-      logger.error(error);
-      await interaction.editReply({ content: `Error: ${error}` });
+    const { factId, text } = await res.json();
+
+    const codes = [400, 401, 404, 500];
+
+    if (codes.includes(res.status)) {
+      await interaction.editReply({ content: text });
       return;
     }
+
+    await interaction.editReply({ content: `\`${factId}\`\n${text}` });
   }
 
   public isGlobalCommand(): boolean {
